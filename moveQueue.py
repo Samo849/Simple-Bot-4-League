12e07b4e7c8314709fe5b1a057e6e723b5f39bff
Samo849 <115582631+Samo849@users.noreply.github.com>
2023-06-14 00:19:40 +0200

    added stop with 9, scaling, stops when game stops

--- moveQueue.py
+++ moveQueue.py
@@ -1,53 +1,98 @@
 import pyautogui
 import time
 import random
+import keyboard
 
 
-# Define the distance to move the cursor
-distance = 30
+num_of_repeats = 100
+stop_execution = False
 
-# Define the number of times to repeat the movement
-num_repeats = 10000
+def on_key_press(event):
+    global stop_execution
+    if event.name == '9':
+        stop_execution = True
+        print("Aborted")
 
-# Move the cursor left and right and click
-for i in range(num_repeats):
-    # Move the cursor to the left
+keyboard.on_press(on_key_press)
 
-    random_numberX = random.randint(1,5)
-    random_numberY = random.randint(1,5)
+screenWidth, screenHeight = pyautogui.size()
 
-    #klikne mapo
-    pyautogui.rightClick(1780 + random_numberX, 943 + random_numberY)
-    pyautogui.rightClick(1780 + random_numberX, 943 + random_numberY)
-    
-    pyautogui.keyDown('alt')
-    pyautogui.press('tab')
-    pyautogui.keyUp('alt')
+screenCenterX = screenWidth / 2
+screenCenterY = screenHeight / 2
 
-    pyautogui.leftClick(1400, 500)
 
-    #accept queue
-    pyautogui.leftClick(930, 650)
 
-    #klikne na par chempov
-    pyautogui.leftClick(1100, 350)
-    pyautogui.leftClick(1050, 350)
-    pyautogui.leftClick(1000, 350)
-    pyautogui.leftClick(950, 350)
-    pyautogui.leftClick(900, 350)
-    pyautogui.leftClick(850, 350)
-    
+for i in range(num_of_repeats): 
 
-    # play again, lock in champ
-    
-    
-    pyautogui.leftClick(930, 720)
-    pyautogui.leftClick(930, 760)
-    pyautogui.leftClick(850, 770)
+    if stop_execution:
+        break
 
-    
+    j = 0
 
-    
-    print(str(i) + ". move \n")
-    time.sleep(3)
 
+    while "League of Legends (TM) Client" in pyautogui.getActiveWindowTitle(): 
+        if stop_execution:
+            break
+
+        league_window = pyautogui.getWindowsWithTitle("League of Legends (TM) Client")[0]
+        league_window_x = league_window.left
+        league_window_y = league_window.top
+        league_window_width = league_window.width
+        league_window_height = league_window.height
+
+                
+        #print(str(i) + "." + str(j))
+        #j += 1
+        
+        random_numberX = random.randint(1,15)
+        random_numberY = random.randint(1,10)
+
+        # clicks on the map
+        pyautogui.rightClick(screenCenterX + league_window_width/4 + league_window_width/8 + league_window_width/32 + league_window_width/128 + random_numberX, 
+                             screenCenterY + league_window_height/4 + league_window_height/16 + league_window_height/32 + league_window_height/128 + random_numberY)
+        
+        pyautogui.rightClick(screenCenterX + league_window_width/4 + league_window_width/8 + league_window_width/32 + league_window_width/128 + random_numberX, 
+                             screenCenterY + league_window_height/4 + league_window_height/16 + league_window_height/32 + league_window_height/128 + random_numberY)
+        
+        
+        # alt tabs because otherwise it doesn't work for some reason
+        pyautogui.keyDown('alt')
+        pyautogui.press('tab')
+        pyautogui.keyUp('alt')
+
+        time.sleep(3)
+        pyautogui.leftClick(screenCenterX + league_window_width/4 + league_window_width/8 + league_window_width/32 + league_window_width/128 + random_numberX, 
+                             screenCenterY + league_window_height/4 + league_window_height/16 + league_window_height/32 + league_window_height/128 + random_numberY)
+
+    while "League of Legends (TM) Client" not in pyautogui.getActiveWindowTitle():
+        if stop_execution:
+            break
+
+
+        #print(str(i) + "." + str(j))
+        #j += 1
+
+        pyautogui.leftClick(1400, 500)
+
+        #accept queue
+        pyautogui.leftClick(930, 650)
+
+        # select a couple of champions
+        pyautogui.leftClick(1100, 350)
+        pyautogui.leftClick(1050, 350)
+        pyautogui.leftClick(1000, 350)
+        pyautogui.leftClick(950, 350)
+        pyautogui.leftClick(900, 350)
+        pyautogui.leftClick(850, 350)
+        
+
+        # play again, lock in, champ
+        
+        
+        pyautogui.leftClick(930, 720)
+        pyautogui.leftClick(930, 760)
+        pyautogui.leftClick(850, 770)
+
+        time.sleep(3)
+
+9
